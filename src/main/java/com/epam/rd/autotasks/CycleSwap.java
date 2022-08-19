329d0ab7fafbb86a62c6885e88e436a8cb83c99e
Yorqinjon Mahmudov <yorqinjonmahmudov93@gmail.com>
2022-08-19 11:50:11 +0000

    Update CycleSwap.java

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -18,25 +18,31 @@ class CycleSwap {
 
     static void cycleSwap(int[] array, int shift) {
 
-        int[] ints = new int[array.length];
-        if(shift > array.length)
-            return;
-        int sub = shift;
-        for (int i = 0; i < shift; i++) {
-            ints[i] = array[array.length-(sub)];
-            sub--;
+            static void cycleSwap(int[] array) {
+        int[] ints;
+        if (array.length > 1) {
+
+
+            int temp = array[array.length-1];
+
+            for (int j = array.length - 1; j > 0; j--)
+                array[j] = array[j - 1];
+
+            array[0] = temp;
         }
-
-
-        int a = array.length-1;
-        for (int i = array.length-shift-1; i >= 0; i--) {
-            array[a] = array[i];
-            a--;
-        }
-
-        for (int i = 0; i < shift; i++)
-            array[i] = ints[i];
 
     }
+
+    static void cycleSwap(int[] array, int shift) {
+        int len = array.length;
+
+        int[] ints = new int[array.length];
+
+
+        for (int i = 0; i < len; i++)
+            ints[(i + shift) % len] = array[i];
+
+                System.arraycopy(ints, 0, array, 0, len);    
+                }
 
 }
