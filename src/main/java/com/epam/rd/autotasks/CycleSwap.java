231fb528a7fb635d15704e7a38e6f94f19fcc6b5
Yorqinjon Mahmudov <yorqinjonmahmudov93@gmail.com>
2022-08-19 11:26:20 +0000

    Update CycleSwap.java

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,40 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        int[] ints;
+        if (array.length > 1) {
+
+
+            int temp = array[array.length-1];
+
+            for (int j = array.length - 1; j > 0; j--)
+                array[j] = array[j - 1];
+
+            array[0] = temp;
+        }
 
     }
 
     static void cycleSwap(int[] array, int shift) {
 
+        int[] ints = new int[array.length];
+
+        int sub = shift;
+        for (int i = 0; i < shift; i++) {
+            ints[i] = array[array.length-(sub)];
+            sub--;
+        }
+
+
+        int a = array.length-1;
+        for (int i = array.length-shift-1; i >= 0; i--) {
+            array[a] = array[i];
+            a--;
+        }
+
+        for (int i = 0; i < shift; i++)
+            array[i] = ints[i];
+
     }
+
 }
